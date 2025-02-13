3acaf001c1791486cd522bea45025e6cadffcdfd
Aavash Subedi <aavashsubedi1@gmail.com>
2025-02-13 22:35:44 +0000

    Fixed code. saved yall life. your welcome. venmo/cashapp me

--- iecdt_lab/DiT.py
+++ iecdt_lab/DiT.py
@@ -1,18 +1,65 @@
-import sys
 import os
-
+import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 import torch
 import torch.nn as nn
+import numpy as np
+import math
+
 from timm.models.vision_transformer import PatchEmbed, Attention, Mlp
-# from src.transformer.timestep_embedding import TimestepEmbedder
 from model_block import modulate, DiTBlock, FinalLayer
 from embedding import embed_2d_sincos_pos
 """
 Although it is a DiT model, as we want the input and output shape to be the same this is fine.
 You can just view it as a autoencoder type of setup.
 """
+
+
+class TimestepEmbedder(nn.Module):
+    """
+    Embeds scalar timesteps into vector representations
+    """
+
+    def __init__(self, hidden_size, frequency_embedding_size=256):
+        super().__init__()
+        self.mlp = nn.Sequential(
+            nn.Linear(frequency_embedding_size, hidden_size, bias=True),
+            nn.SiLU(),
+            nn.Linear(hidden_size, hidden_size, bias=True),
+        )
+        self.frequency_embedding_size = frequency_embedding_size
+
+    @staticmethod
+    def timestep_embedding(t, dim, max_period=10000):
+        """
+        Creates a sinusoidal timestep embedding
+        :param t: a 1-D tensor of N indices, one per batch element. May be frac
+        :param dim: the dimension of the output
+        :param max_period: the minimum frequency of the sinusoids/emebdding
+        :return an (N, D) Tensor of positional embeddings.
+        """
+        # https://github.com/openai/glide-text2im/blob/main/glide_text2im/nn.py
+
+        half = dim // 2
+
+        freqs = torch.exp(
+            - math.log(max_period) * torch.arange(start=0, end=half, dtype=torch.float32) / half
+        ).to(device=t.device)
+
+        args = t[:, None].float() * freqs[None]
+        embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)
+
+        if dim % 2:
+            embedding = torch.cat([embedding, torch.zeros_like(embedding[:, :1])], dim=-1)
+        return embedding
+
+    def forward(self, t):
+        t_freq = self.timestep_embedding(t, self.frequency_embedding_size)
+        t_emb = self.mlp(t_freq)
+        return t_emb
+
+
 class DiT(nn.Module):
     """
     Diffusion model with a Transformer backbone.
@@ -20,7 +67,7 @@ class DiT(nn.Module):
     def __init__(
         self,
         input_size=64,
-        patch_size=2,
+        patch_size=8,
         in_channels=4,
         hidden_size=256,
         depth=28,
@@ -39,7 +86,7 @@ class DiT(nn.Module):
         self.hidden_size = hidden_size
 
         self.x_embedder = PatchEmbed(input_size, patch_size, in_channels, hidden_size, bias=True)
-        # self.t_embedder = TimestepEmbedder(hidden_size)
+        self.t_embedder = TimestepEmbedder(hidden_size)
         #self.y_embedder = LabelEmbedder(num_classes, hidden_size, class_dropout_prob)
         num_patches = self.x_embedder.num_patches
         # Will use fixed sin-cos embedding:
@@ -110,29 +157,31 @@ class DiT(nn.Module):
         y: (N,) tensor of class labels
         """
         x = self.x_embedder(x) + self.pos_embed  # (N, T, D), where T = H * W / patch_size ** 2
-        t = 0 #self.t_embedder(t)                   # (N, D)
+        t = torch.tensor([0.0]).to(x.device) #self.t_embedder(t)                   # (N, D)
+        #repeat t to match the shape of x dim 1
+        t = t.repeat(x.shape[0], 1)
+        t = self.t_embedder(t).squeeze(1)
         #c = t is for any context you want
-        c = t                                 # (N, D)
         for block in self.blocks:
-            x = block(x, c)                      # (N, T, D)
-        x = self.final_layer(x, c)                # (N, T, patch_size ** 2 * out_channels)
+            x = block(x, t)                      # (N, T, D)
+        x = self.final_layer(x, t)                # (N, T, patch_size ** 2 * out_channels)
         x = self.unpatchify(x)                   # (N, out_channels, H, W)
         return x
 
-    def forward_with_cfg(self, x, t, cfg_scale):
-        """
-        Forward pass of DiT, but also batches the unconditional forward pass for classifier-free guidance.
-        """
-        # https://github.com/openai/glide-text2im/blob/main/notebooks/text2im.ipynb
-        half = x[: len(x) // 2]
-        combined = torch.cat([half, half], dim=0)
-        model_out = self.forward(combined, t)#, y)
-        # For exact reproducibility reasons, we apply classifier-free guidance on only
-        # three channels by default. The standard approach to cfg applies it to all channels.
-        # This can be done by uncommenting the following line and commenting-out the line following that.
-        # eps, rest = model_out[:, :self.in_channels], model_out[:, self.in_channels:]
-        eps, rest = model_out[:, :3], model_out[:, 3:]
-        cond_eps, uncond_eps = torch.split(eps, len(eps) // 2, dim=0)
-        half_eps = uncond_eps + cfg_scale * (cond_eps - uncond_eps)
-        eps = torch.cat([half_eps, half_eps], dim=0)
-        return torch.cat([eps, rest], dim=1)
+    # def forward_with_cfg(self, x, t, cfg_scale):
+    #     """
+    #     Forward pass of DiT, but also batches the unconditional forward pass for classifier-free guidance.
+    #     """
+    #     # https://github.com/openai/glide-text2im/blob/main/notebooks/text2im.ipynb
+    #     half = x[: len(x) // 2]
+    #     combined = torch.cat([half, half], dim=0)
+    #     model_out = self.forward(combined, t)#, y)
+    #     # For exact reproducibility reasons, we apply classifier-free guidance on only
+    #     # three channels by default. The standard approach to cfg applies it to all channels.
+    #     # This can be done by uncommenting the following line and commenting-out the line following that.
+    #     # eps, rest = model_out[:, :self.in_channels], model_out[:, self.in_channels:]
+    #     eps, rest = model_out[:, :3], model_out[:, 3:]
+    #     cond_eps, uncond_eps = torch.split(eps, len(eps) // 2, dim=0)
+    #     half_eps = uncond_eps + cfg_scale * (cond_eps - uncond_eps)
+    #     eps = torch.cat([half_eps, half_eps], dim=0)
+    #     return torch.cat([eps, rest], dim=1)
